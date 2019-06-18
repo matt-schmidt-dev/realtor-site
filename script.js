cc2edad5afbbadd569b063eeff055f5b24688404
Matthew Schmidt <47067179+matts0809@users.noreply.github.com>
2019-06-18 14:25:56 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //PARALLAX
-
+/*
 $(window).scroll(function() {
   parallax();
 });
@@ -11,6 +11,7 @@ function parallax() {
 
   $('.parallax--bg').css('background-position', 'center '+(wScroll*0.5)+'px');
 }
+*/
 
 
 $(document).ready(function() {
@@ -58,55 +59,31 @@ document.querySelector('.burger-exit').classList.toggle('active-exit-burger');
 
 //Scroll Animation - Navigation
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
+  if ($(document).scrollTop() > 250) {
     $('#nav-container-box').addClass('nav-bg-scroll');
-    $('.ind-nav-link').addClass('nav-text-scroll');
     $('.logo-image').addClass('logo-image-scroll');
-    $('.burger div').addClass('burger-scroll');
-    $('.ind-nav-link').addClass('ind-nav-link-scroll');
   }
   else {
     $('#nav-container-box').removeClass('nav-bg-scroll');
-    $('.ind-nav-link').removeClass('nav-text-scroll');
     $('.logo-image').removeClass('logo-image-scroll');
-    $('.burger div').removeClass('burger-scroll');
-    $('.ind-nav-link').removeClass('ind-nav-link-scroll');
   }
 });
+
 
 
 //Scroll Animation for Services Section
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 300) {
-    $('.s1-des, .s1-img').addClass('s123-scroll');
-  }
-  else {
-    $('.s1-des, .s1-img').removeClass('s123-scroll');
-  }
-});
-
-$(window).scroll(function() {
   if ($(document).scrollTop() > 650) {
-    $('.s2-des, .s2-img').addClass('s123-scroll');
+    $('.about-des, .about-img').addClass('about-scroll');
   }
-  else {
-    $('.s2-des, .s2-img').removeClass('s123-scroll');
-  }
-});
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 1000) {
-    $('.s3-des, .s3-img').addClass('s123-scroll');
-  }
-  else {
-    $('.s3-des, .s3-img').removeClass('s123-scroll');
-  }
+  /*else {
+    $('.about-des, .about-img').removeClass('about-scroll');
+  }*/
 });
 
 
 
-
-$('.single-item').slick({
+$('.single-item-head').slick({
 	speed: 500,
 	fade: true,
   cssEase: 'linear',
@@ -114,13 +91,10 @@ $('.single-item').slick({
   autoplaySpeed: 5000,
 });
 
-
-
-
-/*
-$('.autoplay').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});*/
+$('.single-item-test').slick({
+	speed: 500,
+	fade: false,
+  cssEase: 'linear',
+	autoplay: true,
+  autoplaySpeed: 3000,
+});
