f8ce075cc7059d954a3f7148b517bb1eb8f2a586
Matthew Schmidt <47067179+matts0809@users.noreply.github.com>
2019-09-11 15:08:10 -0500

    upadated all files

--- script.js
+++ script.js
@@ -1,3 +1,110 @@
+//Google Map Listsings
+function initMap() {
+
+  // Map options
+  var options = {
+    center: {
+      lat: 29.834539,
+      lng: -95.469543
+    },
+    zoom: 11
+  }
+
+  // New Map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+
+  // Array of Markers
+
+  var markers = [{
+    //List-One
+    coords: {
+      lat: 29.826670,
+      lng: -95.440063
+    },
+    content: '<a href="list-one.html"><h5 class="map-marker-content-title">1546 Gardenia Drive</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Gardenia-img-1.jpg" alt=""/><div class="map-info"><h5>$1,025,000</h5><h6>4000sq-ft<br>4-Bed<br>3.5-Bath<br></h6></div></div><a/>'
+  }, {
+    //List-Two
+    coords: {
+      lat: 29.802740,
+      lng: -95.439810
+    },
+    content: '<a href="list-two.html"><h5 class="map-marker-content-title">1815 Willowby Drive</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Millwood-img-1.jpg" alt=""/><div class="map-info"><h5>$435,000</h5><h6>1822sq-ft<br>3-Bed<br>2-Bath<br></h6></div></div><a/>'
+  }, {
+    //List-Three
+    coords: {
+      lat: 29.800940,
+      lng: -95.432620
+    },
+    content: '<a href="list-three.html"><h5 class="map-marker-content-title">1822 Millwood Drive</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Willowby-img-1.jpg" alt=""/><div class="map-info"><h5>$510,000</h5><h6>1848sq-ft<br>3-Bed<br>2-Bath<br></h6></div></div><a/>'
+  }, {
+    //List-Four
+    coords: {
+      lat: 29.837339,
+      lng: -95.449348
+    },
+    content: '<a href="list-four.html"><h5 class="map-marker-content-title">4402 Fallbrook Drive</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Fallbrook-img-1.jpg" alt=""/><div class="map-info"><h5>$419,900</h5><h6>1737sq-ft<br>3-Bed<br>2-Bath<br></h6></div></div><a/>'
+  }, {
+    //List-Five
+    coords: {
+      lat: 29.903530,
+      lng: -95.562202
+    },
+    content: '<a href="list-five.html"><h5 class="map-marker-content-title">9415 Stone Porch Lane</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Stone-img-1.jpg" alt=""/><div class="map-info"><h5>$379,000</h5><h6>3772sq-ft<br>4-Bed<br>3.5-Bath<br></h6></div></div><a/>'
+  }, {
+    //List-Six
+    coords: {
+      lat: 29.834539,
+      lng: -95.469543
+    },
+    content: '<a href="list-six.html"><h5 class="map-marker-content-title">5221 Viking Drive</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Viking-img-1.jpg" alt=""/><div class="map-info"><h5>$269,000</h5><h6>1414sq-ft<br>3-Bed<br>1.5-Bath<br></h6></div></div><a/>'
+  }, {
+    //List-Seven
+    coords: {
+      lat: 29.907460,
+      lng: -95.567670
+    },
+    content: '<a href="list-seven.html"><h5 class="map-marker-content-title">2207 Brimberry Street</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Chester-img-1.jpg" alt=""/><div class="map-info"><h5>$1795/month</h5><h6>1206sq-ft<br>2-Bed<br>1-Bath<br></h6></div></div><a/>'
+  }, {
+    //List-Eight
+    coords: {
+      lat: 29.833490,
+      lng: -95.466460
+    },
+    content: '<a href="list-eight.html"><h5 class="map-marker-content-title">1546 Gardenia Drive</h5><div class="map-marker-content-div"><img class="map-img-tiny" src="css/images/Lido-img-1.jpg" alt=""/><div class="map-info"><h5>$269,000</h5><h6>1414sq-ft<br>3-Bed<br>1.5-Bath<br></h6></div></div><a/>'
+  }]
+
+
+  // Loop Through markers
+  for (var i = 0; i < markers.length; i++) {
+    //Add Marker
+    addMarker(markers[i]);
+  }
+
+  // Add Marker Function
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+    });
+
+    if (props.content) {
+
+      // Add Info Window
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content
+      });
+
+      marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+}
+
+
+
+
 //PARALLAX
 /*
 $(window).scroll(function() {
@@ -37,6 +144,7 @@ document.querySelector('.nav-ul-list').classList.toggle('nav-active');
 document.querySelector('.burger').addEventListener('click', () => {
 document.querySelector('.burger-exit').classList.toggle('active-exit-burger');
   	});
+
 
 
     $(document).ready(function(){
@@ -104,6 +212,7 @@ $('.single-item-head').slick({
   autoplaySpeed: 5000,
 });
 
+/*
 //Testimonial-Carousel
 $('.single-item-test').slick({
 	speed: 500,
@@ -114,12 +223,13 @@ $('.single-item-test').slick({
   arrows: false,
   dots: true,
 });
+*/
 
 //ind list page sliders
 $('.slider-for').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
- arrows: false,
+ arrows: true,
  fade: true,
  asNavFor: '.slider-nav'
 });
@@ -127,7 +237,32 @@ $('.slider-nav').slick({
  slidesToShow: 5,
  slidesToScroll: 1,
  asNavFor: '.slider-for',
- dots: true,
+ dots: false,
+ arrows: false,
  centerMode: true,
  focusOnSelect: true
 });
+
+
+
+
+/*
+$(document).ready(function(){
+  var listOneAddress = document.querySelector('#list-one-address');
+  var listOneBaths = document.querySelector('#list-one-baths');
+  var listOneBeds = document.querySelector('#list-one-beds');
+  var listOneSqFt = document.querySelector('#list-one-sq-ft');
+
+
+document.querySelector('#import-l1-address').textContent = listOneAddress;
+document.querySelector('#import-l1-baths').innerHTML = listOneBaths;
+document.querySelector('#import-l1-beds').innerHTML = listOneBeds;
+document.querySelector('#import-l1-sq-ft').innerHTML = listOneSqFt;
+});
+*/
+
+
+/*
+//MLS-IDS-Listings
+$('#div-ex').load('https://search.har.com/idx/dosearch.cfm?sitetype=aws&cid=238237&allmls=n');
+*/
